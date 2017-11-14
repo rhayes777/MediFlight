2f0c916a620b326c77fc23d56fbe4f577253fd2b
Richard <richardhayes777@gmail.com>
2017-11-14 10:10:09 +0000

    moved objects

---
+++ MediFlight/Time.swift
@@ -0,0 +1,16 @@
+//
+//  TimeZone.swift
+//  MediFlight
+//
+//  Created by Richard Hayes on 14/11/2017.
+//  Copyright © 2017 Farming Online. All rights reserved.
+//
+
+import UIKit
+
+class TimeZone: NSObject {
+    
+//    You can set the default time zone within your application using setDefaultTimeZone:. You can access this default time zone at any time with the defaultTimeZone class method. With the localTimeZone class method you can get a time zone object that automatically updates itself to reflect changes to the default time zone.
+    
+
+}
